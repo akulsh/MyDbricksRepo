d62102fefd76a6db6e4b1c0f2c76e0f5585e8b0e
akulsh <akulsh>
2023-09-02 15:49:56 +0000

    Added New Example
    Added New Example

---
+++ NoteBooks/Untitled Notebook 2023-08-27 03_55_08.sql
@@ -0,0 +1,16 @@
+-- Databricks notebook source
+-- MAGIC %python
+-- MAGIC print("Sample Notebook")
+
+-- COMMAND ----------
+
+-- MAGIC %python
+-- MAGIC 1+2
+
+-- COMMAND ----------
+
+select current_date()
+
+-- COMMAND ----------
+
+select current_schema()
